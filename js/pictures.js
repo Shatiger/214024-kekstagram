d2daf56dc7962d7c529edc77459ad0204a1b04fa
Shatiger <shatiger@yandex.ru>
2018-06-24 22:22:20 +0800

    Выполнено задание 4-2

--- js/pictures.js
+++ js/pictures.js
@@ -263,28 +263,28 @@ var onHashtagInput = function (evt) {
     target.setCustomValidity('');
   } else {
     var hashtags = inputValue.split(' ');
-    for (var i = 0; i < hashtags.length; i++) {
-      hashtags[i] = hashtags[i].toLowerCase();
+    for (var m = 0; m < hashtags.length; m++) {
+      hashtags[m] = hashtags[m].toLowerCase();
     }
     if (hashtags.length > 5) {
       target.setCustomValidity('Нельзя указывать больше 5 хэштегов!');
     } else {
-      for (var i = 0; i < hashtags.length; i++) {
-        if (hashtags[i].charAt(0) !== '#') {
+      for (var n = 0; n < hashtags.length; n++) {
+        if (hashtags[n].charAt(0) !== '#') {
           target.setCustomValidity('Хэштеги должны начинаться с символа "#"!');
           isValid = false;
           break;
-        } else if (hashtags[i].length === 1) {
+        } else if (hashtags[n].length === 1) {
           target.setCustomValidity('Хэштег должен иметь не только лишь символ "#"!');
           isValid = false;
           break;
-        } else if (hashtags[i].length > 20) {
+        } else if (hashtags[n].length > 20) {
           target.setCustomValidity('Максимальная длина хэштега - 20 символов, включая символ "#"!');
           isValid = false;
           break;
         }
-        for (var j = 0; j < hashtags.length; j++) {
-          if (i !== j && hashtags[j] === hashtags[i]) {
+        for (var h = 0; h < hashtags.length; h++) {
+          if (n !== h && hashtags[h] === hashtags[n]) {
             target.setCustomValidity('Не должно быть одинаковых хэштегов! Хэштеги не чувствительны к регистру!');
             isValid = false;
             break;
